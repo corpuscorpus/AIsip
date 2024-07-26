24e40cc11b7ae5b289d373e39cb166f9c678f470
corpuscorpus <122154206+corpuscorpus@users.noreply.github.com>
2024-07-26 08:58:22 +0200

    Update

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setupChart();
+    setup3DScene();
+    setupAsciiArt();
+    setupThemeSwitcher();
+    startSessionTimer();
+});
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+// Additional functionality implementations (e.g., session timers, API interactions)
